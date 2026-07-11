512b50337a81e2cb6e193ff0d8218711d40e3734
ygftlp <ygftlp@163.com>
2026-07-11 12:11:18 +0800

    install frontend presentation isolation in wechat entry

--- src/main.wx.ts
+++ src/main.wx.ts
@@ -1,3 +1,5 @@
 import { startWeChatRacerGame } from './platforms/wechat/startup';
+import { installRacerMenuPresentation } from './racer/RacerMenuPresentation';
 
+installRacerMenuPresentation();
 startWeChatRacerGame();
