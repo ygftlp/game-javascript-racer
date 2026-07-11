a88fb86626a4d75e36e49b65f72f1de01cbec6ae
ygftlp <ygftlp@163.com>
2026-07-11 16:42:59 +0800

    remove legacy menu presentation patch from wechat entry

--- src/main.wx.ts
+++ src/main.wx.ts
@@ -1,7 +1,5 @@
 import { ensureWeChatMainCanvas } from './platforms/wechat/RacerWechatMainCanvas';
 import { startWeChatRacerGame } from './platforms/wechat/startup';
-import { installRacerMenuPresentation } from './racer/RacerMenuPresentation';
 
 ensureWeChatMainCanvas();
-installRacerMenuPresentation();
 startWeChatRacerGame();
