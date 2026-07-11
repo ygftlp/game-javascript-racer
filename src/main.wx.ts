ea03614b980fdb1485c082c742ddae77b2eab5a9
ygftlp <ygftlp@163.com>
2026-07-11 15:30:23 +0800

    initialize the WeChat main canvas before the game engine

--- src/main.wx.ts
+++ src/main.wx.ts
@@ -1,5 +1,7 @@
+import { ensureWeChatMainCanvas } from './platforms/wechat/RacerWechatMainCanvas';
 import { startWeChatRacerGame } from './platforms/wechat/startup';
 import { installRacerMenuPresentation } from './racer/RacerMenuPresentation';
 
+ensureWeChatMainCanvas();
 installRacerMenuPresentation();
 startWeChatRacerGame();
